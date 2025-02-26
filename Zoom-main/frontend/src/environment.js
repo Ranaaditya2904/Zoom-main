e4f9c740d26dc452fd02ea4b61d2160712e79ea2
Aditya Kumar Rana <ranaaditya2904@gmail.com>
2025-02-26 20:37:49 +0530

    Deploying

--- Zoom-main/frontend/src/environment.js
+++ Zoom-main/frontend/src/environment.js
@@ -1,6 +1,6 @@
-// let IS_PROD = true;
-// const server = IS_PROD ?
-//  "https://apnacollegebackend.onrender.com" :
-//     "http://localhost:8000"
+let IS_PROD = true;
+const server = IS_PROD ?
+ "https://zoom-main-1fronend.onrender.com" :
+    "http://localhost:8000"
 
-// export default server;
+export default server;
