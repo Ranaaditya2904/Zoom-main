cb420cbdd4c33f2fbdbc049f8e2324560f473b30
Aditya Kumar Rana <137464802+Ranaaditya2904@users.noreply.github.com>
2025-02-26 21:49:08 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://ranaaditya2904:Aditya12345@cluster0.wvrlo.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://ranaaditya2904:Aditya12345@cluster0.wvrlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
