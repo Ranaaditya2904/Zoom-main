b692595d4f2359c474df9f4f82dbd72e9b349c4a
Aditya Kumar Rana <137464802+Ranaaditya2904@users.noreply.github.com>
2025-03-20 22:09:42 +0530

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,7 +26,7 @@ function HomeComponent() {
 
                 <div style={{ display: "flex", alignItems: "center" }}>
 
-                    <h2>Apna Video Call</h2>
+                    <h2>AdyTalk</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
@@ -54,7 +54,7 @@ function HomeComponent() {
             <div className="meetContainer">
                 <div className="leftPanel">
                     <div>
-                        <h2>Providing Quality Video Call Just Like Quality Education</h2>
+                        <h2>AdiConnect is a high-quality video calling application that enables seamless communication through video calls, text messaging, and screen sharing. Built for personal and professional use, it ensures a smooth and secure experience.</h2>
 
                         <div style={{ display: 'flex', gap: "10px" }}>
 
